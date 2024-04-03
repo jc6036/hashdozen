dba23a09f3bc9cc8440887655d83dec6ec7fc917
Jesse Cabell <jc6036@gmail.com>
2024-04-03 19:51:52 -0400

    Added salting method for vastly enhanced avalanche and less collision.

--- src/hashdozen.rs
+++ src/hashdozen.rs
@@ -2,69 +2,44 @@
 // TODO: Add tests
 // TODO: Add support for HashMap
 // TODO: impl Hasher for all rust primitives
+// TODO: Remove salt, add utility method to generate a salted value as an option
+// TODO: Alter the pad to cut down on repeated pad values
 
 // Primary Hasher Implementation
-fn run_hash (mut input: (Vec<u8>, Vec<u8>)) -> String {
+fn run_hash (mut input: Vec<u8>) -> String {
     // Pad out to multiple of 6 bytes
     pad (&mut input);
 
     // Compress each vec into one block
     let datablocks = compress(input);
 
-    // Finally, combine the main data block and the salt
-    let finaldata = combine(datablocks);
-
     // Now output as a string
-    return format_hash(finaldata);
+    return format_hash(datablocks);
 }
 
-fn pad(data: &mut (Vec<u8>, Vec<u8>)) {
-    data.0.extend(data.0.len().to_be_bytes());
-    while data.0.len() % 6 > 0 {
-        data.0.push(255);
-    }
-
-    data.1.extend(data.1.len().to_be_bytes());
-    while data.1.len() % 6 > 0 {
-        data.1.push(255);
+fn pad(data: &mut Vec<u8>) {
+    data.extend(data.len().to_be_bytes());
+    while data.len() % 6 > 0 {
+        data.push(255);
     }
 }
 
-fn compress(mut data: (Vec<u8>, Vec<u8>)) -> (Vec<u8>, Vec<u8>) {
-    let mut mainblock: Vec<u8> = data.0[data.0.len() - 6..].to_vec();
-    let mut saltblock: Vec<u8> = data.1[data.1.len() - 6..].to_vec();
+fn compress(mut data: Vec<u8>) -> Vec<u8> {
+    let mut mainblock: Vec<u8> = data[data.len() - 6..].to_vec();
 
-    data.0.truncate(data.0.len() - 6);
+    data.truncate(data.len() - 6);
 
-    data.1.truncate(data.1.len() - 6);
-    while data.0.len() >= 6 {
-        // XOR the first 6 bytes of data into our datablock
+    while data.len() >= 6 {
         mainblock = mainblock
                         .iter()
-                        .zip(data.0[data.0.len() - 6..].iter())
+                        .zip(data[data.len() - 6..].iter())
                         .map(|(&x1, &x2)| ((x1.rotate_right(x2.into())) ^ (x2.rotate_right(x1.into()))))
                         .collect();
-        data.0.truncate(data.0.len() - 6);
+        data.truncate(data.len() - 6);
     }
-    while data.1.len() >= 6 {
-        saltblock = saltblock
-                        .iter()
-                        .zip(data.1[data.1.len() - 6..].iter())
-                        .map(|(&x1, &x2)| ((x1.rotate_right(x2.into())) ^ (x2.rotate_right(x1.into()))))
-                        .collect();
-        data.1.truncate(data.1.len() - 6);
-    }
-
-    return (mainblock, saltblock);
+    return mainblock;
 }
 
-fn combine (data: (Vec<u8>, Vec<u8>)) -> Vec<u8> {
-    data.0
-        .iter()
-        .zip(data.1.iter())
-        .map(|(&x1, &x2)| ((x1.rotate_right(x2.into())) ^ (x2.rotate_right(x1.into()))))
-        .collect()
-}
 
 fn format_hash(data: Vec<u8>) -> String {
     let mut outstr = String::new();
@@ -101,14 +76,15 @@ impl Hasher for usize {
     }
 }
 
-// API
-// This trait must be implemented for any type you wish to be compatible with the hasher
+impl Hasher for Vec<u8> {
+    fn convert_to_bytes(&self) -> Vec<u8> {
+        self.clone()
+    }
+}
+
+
 /// This needs implemented in order to run hash on your custom types. 
 /// Already implemented for rust primitives and String.
-/// 
-/// ```
-/// // Example of how you might implement this to work on a simple struct
-/// ```
 pub trait Hasher {
     fn convert_to_bytes (&self) -> Vec<u8>;
 }
@@ -116,26 +92,29 @@ pub trait Hasher {
 /// This is the primary public interface for HashDozen.
 /// 
 /// The function declaration should be fairly self explanatory.
-/// You may notice that the types are trait bounded to Hasher. If you have custom types you wish to
+/// You may notice that the type is trait bounded to Hasher. If you have custom types you wish to
 /// implement, please see the documentation for the trait.
-/// 
-/// It expects an input and a salt, every time. It's up to you how to generate the salt, and they can 
-/// be different types.
-/// 
-/// ```
-/// // Usage Example
-/// let test1 = 23890423984;
-/// let test2 = String::from("Test2asfasfdsaf");
-/// println!("{}",hashdozen::hash(&test1, &test2).unwrap()); 
-/// ```
-pub fn hash<'a, D: Hasher, S: Hasher> (input: &D, salt: &S) -> Result<String, &'a str> {
-    let hashinput = input.convert_to_bytes();
-    let saltinput = salt.convert_to_bytes();
+pub fn hash<'a, T: Hasher> (input: &T) -> Result<String, &'a str> {
+    let hashinput = input.convert_to_bytes();    
 
-    if hashinput.len() == 0 || saltinput.len() == 0 {
-        let ret: &'a str = "One or more inputs were empty.";
+    if hashinput.len() == 0 {
+        let ret: &'a str = "Input can not be empty.";
         return Result::Err(ret);
     }
 
-    return Ok(run_hash((hashinput, saltinput)));
+    return Ok(run_hash(hashinput));
+}
+
+/// Takes a data value that must implement Hasher and convert_to_bytes, then returns it salted.
+/// 
+/// Recommended to run this when creating input for the hasher, but feel free to replace it with whatever
+/// salting process you wish.
+pub fn generate_salt<T: Hasher> (input: &T) -> Vec<u8> {
+    let mut input = input.convert_to_bytes();
+
+    let salt: Vec<u8> = input.iter().zip(input.iter().rev()).map(|(x1, x2)| (x1 | x2) ^ (x1 & x2)).collect();
+
+    input.append(&mut salt.clone());
+
+    input
 }
